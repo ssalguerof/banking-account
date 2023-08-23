36705cc16373322f2ba8e04d2c0179b7632ccf4e
ssalguero <ssalguerof@gmail.com>
2023-08-23 10:43:19 -0500

    controller

---
+++ banking-account/account.cmd/src/main/java/com/banking/account/cmd/api/controllers/CloseAccountController.java
@@ -0,0 +1,41 @@
+package com.banking.account.cmd.api.controllers;
+
+import com.banking.account.cmd.api.command.CloseAccountCommand;
+import com.banking.account.cmd.api.command.DepositFundsCommand;
+import com.banking.account.common.dto.BaseResponse;
+import com.banking.cqrs.core.exceptions.AggregateNotFoundException;
+import com.banking.cqrs.core.infrastructure.CommandDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.MessageFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(path = "/api/v1/closeBankAccount")
+public class CloseAccountController {
+
+    private final Logger logger = Logger.getLogger(CloseAccountController.class.getName());
+
+    @Autowired
+    private CommandDispatcher commandDispatcher;
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<BaseResponse> closeAccount(@PathVariable(value = "id") String id){
+        try {
+            CloseAccountCommand command = new CloseAccountCommand(id);
+            commandDispatcher.send(command);
+            return new ResponseEntity<>(new BaseResponse("Se cerro la cuenta bancaria exitosamente"), HttpStatus.OK);
+
+        }catch (IllegalStateException | AggregateNotFoundException e){
+            logger.log(Level.WARNING, MessageFormat.format("El cliente envio un request con errores {0} ", e.toString()));
+            return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            var safeErrorMessage = MessageFormat.format("Errores mientras procesaba el request {id}", id);
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ banking-account/account.cmd/src/main/java/com/banking/account/cmd/api/controllers/WithdrawFundsController.java
@@ -0,0 +1,43 @@
+package com.banking.account.cmd.api.controllers;
+
+import com.banking.account.cmd.api.command.DepositFundsCommand;
+import com.banking.account.cmd.api.command.WithdrawFundsCommand;
+import com.banking.account.common.dto.BaseResponse;
+import com.banking.cqrs.core.exceptions.AggregateNotFoundException;
+import com.banking.cqrs.core.infrastructure.CommandDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.MessageFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(path = "/api/v1/withDrawFunds")
+public class WithdrawFundsController {
+
+    private final Logger logger = Logger.getLogger(WithdrawFundsController.class.getName());
+
+    @Autowired
+    private CommandDispatcher commandDispatcher;
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<BaseResponse> withDrawFunds(@PathVariable(value = "id") String id,
+                                                     @RequestBody WithdrawFundsCommand command){
+        try {
+            command.setId(id);
+            commandDispatcher.send(command);
+            return new ResponseEntity<>(new BaseResponse("El retiro de dinero fue exitoso"), HttpStatus.OK);
+
+        }catch (IllegalStateException | AggregateNotFoundException e){
+            logger.log(Level.WARNING, MessageFormat.format("El cliente envio un request con errores {0} ", e.toString()));
+            return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            var safeErrorMessage = MessageFormat.format("Errores mientras procesaba el request {id}", id);
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
